d7e29ba39809591d1040b63d1ade918cca2803fb
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2018-11-25 22:54:13 +0100

    Initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
